f3f41f3b2cab192b465b6a12600d1db8885db211
Thamindu Dasanayake <thamindudasanayake@gmail.com>
2025-12-07 19:04:18 +0530

    implement dock presentation

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,7 +5,7 @@ import {useGSAP} from "@gsap/react";
 const FONT_WEIGHTS = {
     subtitle: {min: 100, max: 400, default: 100},
     title: {min: 400, max: 900, default: 400},
-}
+};
 
 const renderText = (text, className, baseWeight = 400) => {
     return [...text].map((char, i) => (<span
@@ -14,46 +14,46 @@ const renderText = (text, className, baseWeight = 400) => {
         style={{fontVariationSettings: `'wght' ${baseWeight}`}}
     >
             {char === " " ? "\u00A0" : char}
-        </span>))
-}
+        </span>));
+};
 
 const setupTextHover = (container, type) => {
     if (!container) return () => {
     };
 
-    const letters = container.querySelectorAll("span")
-    const {min, max, default: base} = FONT_WEIGHTS[type]
+    const letters = container.querySelectorAll("span");
+    const {min, max, default: base} = FONT_WEIGHTS[type];
 
     const animateLetter = (letter, weight, duration = 0.25) => {
         return gsap.to(letter, {duration, ease: "power2.out", fontVariationSettings: `'wght' ${weight}`});
-    }
+    };
 
     const handleMouseMove = (e) => {
         const {left} = container.getBoundingClientRect();
-        const mouseX = e.clientX - left
+        const mouseX = e.clientX - left;
 
         letters.forEach((letter) => {
             const {left: l, width: w} = letter.getBoundingClientRect();
-            const distance = Math.abs(mouseX - (l - left + w / 2))
-            const intensity = Math.exp(-(distance ** 2) / 20000)
+            const distance = Math.abs(mouseX - (l - left + w / 2));
+            const intensity = Math.exp(-(distance ** 2) / 20000);
 
 
-            animateLetter(letter, min + (max - min) * intensity)
-        })
-    }
+            animateLetter(letter, min + (max - min) * intensity);
+        });
+    };
 
     const handleMouseLeave = (e) => {
-        letters.forEach((letter) => animateLetter(letter, base, 0.3))
-    }
+        letters.forEach((letter) => animateLetter(letter, base, 0.3));
+    };
 
-    container.addEventListener("mousemove", handleMouseMove)
-    container.addEventListener("mouseleave", handleMouseLeave)
+    container.addEventListener("mousemove", handleMouseMove);
+    container.addEventListener("mouseleave", handleMouseLeave);
 
     return () => {
-        container.removeEventListener("mousemove", handleMouseMove)
-        container.removeEventListener("mouseleave", handleMouseLeave)
+        container.removeEventListener("mousemove", handleMouseMove);
+        container.removeEventListener("mouseleave", handleMouseLeave);
     };
-}
+};
 
 const Welcome = () => {
     const titleRef = useRef(null);
@@ -64,10 +64,10 @@ const Welcome = () => {
         const subtitleCleanup = setupTextHover(subtitleRef.current, 'subtitle');
 
         return () => {
-            subtitleCleanup()
-            titleCleanup()
-        }
-    })
+            subtitleCleanup();
+            titleCleanup();
+        };
+    }, []);
 
     return (<section id="welcome">
         <p ref={subtitleRef}>
@@ -80,6 +80,6 @@ const Welcome = () => {
         <div className="small-screen">
             <p>This Portfolio is designed for desktop/tablet screens only.</p>
         </div>
-    </section>)
-}
-export default Welcome
+    </section>);
+};
+export default Welcome;
