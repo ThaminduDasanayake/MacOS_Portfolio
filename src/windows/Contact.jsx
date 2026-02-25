c9e10fd6dc7e4ddc4b86f6118c001bf39edaff0a
Thamindu Dasanayake <thamindudasanayake@gmail.com>
2026-02-25 15:05:08 +0530

    Updated content

--- src/windows/Contact.jsx
+++ src/windows/Contact.jsx
@@ -1,33 +1,38 @@
 import WindowWrapper from "#hoc/WindowWrapper.jsx";
-import {WindowControls} from "#components/index.js";
-import {socials} from "#constants/index.js";
+import { WindowControls } from "#components/index.js";
+import { socials } from "#constants/index.js";
 
 const Contact = () => {
-    return (
-        <>
-            <div id="window-header">
-                <WindowControls target="contact"/>
-                <h2>Contact me</h2>
-            </div>
+  return (
+    <>
+      <div id="window-header">
+        <WindowControls target="contact" />
+        <h2>Contact me</h2>
+      </div>
 
-            <div className="p-5 space-y-5">
-                <img src="/images/adrian.jpg" alt="Thamindu" className="w-20 rounded-full"/>
+      <div className="p-5 space-y-5">
+        <img
+          src="/images/pic.jpg"
+          alt="Thamindu"
+          className="w-20 rounded-full"
+        />
 
-                <h3>Let's Connect</h3>
-                <p>Got an idea? A bug to squash? Or just wanna talk tech? I'm in.</p>
+        <h3>Let's Connect</h3>
+        <p>Got an idea? A bug to squash? Or just wanna talk tech? I'm in.</p>
 
-                <ul>{socials.map(({id, bg, link, icon, text}) => (
-                    <li key={id} style={{backgroundColor: bg}}>
-                        <a href={link} target="_blank" rel="noopener noreferrer">
-                            <img src={icon} alt={text} className="size-5"/>
-                            <p>{text}</p>
-                        </a>
-                    </li>
-                ))}</ul>
-            </div>
-
-        </>
-    );
+        <ul>
+          {socials.map(({ id, bg, link, icon, text }) => (
+            <li key={id} style={{ backgroundColor: bg }}>
+              <a href={link} target="_blank" rel="noopener noreferrer">
+                <img src={icon} alt={text} className="size-5" />
+                <p>{text}</p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 const ContactWindow = WindowWrapper(Contact, "contact");
